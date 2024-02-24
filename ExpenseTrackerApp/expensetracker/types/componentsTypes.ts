f7339585aa4bb118ed74260a539e7bcbe3579968
Muhammad Affan Sheikh <111854056+iaffansheikh21@users.noreply.github.com>
2024-02-24 18:37:30 +0500

    Expense tracker App using React Recharts

---
+++ ExpenseTrackerApp/expensetracker/types/componentsTypes.ts
@@ -0,0 +1,17 @@
+import { ExpenseType } from "./commonTypes"
+
+// export type ExpensePropsType = {
+//     note: string,
+//     date: string,
+//     amount: number,
+//     category: string,
+//     id: string
+// }
+
+export type ExpenseModalProps = {
+    isOpen: boolean,
+    onClose: ()=> void,
+    expense: ExpenseType
+    onAddExpense: (expense:ExpenseType)=> void
+    onUpdateExpense: (expense:ExpenseType)=> void
+}
